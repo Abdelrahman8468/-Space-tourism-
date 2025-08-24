874ceaa5b16ec12177e14bf7b945bb694085630c
Abdelrahman <abdelrahmanismail673@gmail.com>
2025-08-24 15:31:59 +0300

    Initial commit

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -50,17 +50,19 @@ function changeTabPanel(e) {
         
     targetTab.setAttribute("aria-selected", true);
     
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute("hidden", true));
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
     
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
-    
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture) => picture.setAttribute("hidden", true));
-        
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-    
-    // console.log(mainContainer);
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+     parent.querySelector(content).removeAttribute('hidden');
 }
